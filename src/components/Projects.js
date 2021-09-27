ddcdb76e6e0c97b4b5f88ee4bf0fdc710adb4f0d
Shaishav Shah <shaishav.shah04@gmail.com>
2021-09-27 07:21:42 -0600

    The projects section is done.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,62 @@
 import React from 'react'
 import "../styles/Projects.css";
+import Link from '@mui/icons-material/Link';
 
 function Projects() {
 
     const ProjectPageInfo = [
     {
-        "proj-name": "",
-        "proj-tech":"",
-        "proj-description":"",
-        "proj-link":"",
-        "proj-img": ""
+        "proj-name": "Algorithmic CryptoCurrency Scanner",
+        "proj-description":["A script running on a EC2 instance that scans the cryptocurrency markets hourly. It fetches data from exchanges and analyzes the data for potential bullish indicators.","The results are compared with previously issued alerts to reduce redundancy, and are texted to my number."],
+        "proj-tech":"Python, AWS (Hosting), CCXT Library (Data), Twilio Client (Messages) ",
+        "proj-link":"https://github.com/ShaishavShah04/Crypto-Scanner",
+        "proj-img": "src/media/scanner.png",
+    }, 
+    {
+        "proj-name": "Cash Flow Management App",
+        "proj-description":["This is an android app that utilizes Firebase's OCR model to scan reciepts and summarize the spending habits in form of a pie chart."],
+        "proj-tech":"Java, Firebase, React Native, Machine Learning (OCR)",
+        "proj-link":"https://github.com/ShaishavShah04/Crypto-Scanner",
+        "proj-img": "src/media/scanner.png",
+    },
+    {
+        "proj-name": "Stock Widget",
+        "proj-description":["A script running on a EC2 instance that scans the cryptocurrency markets hourly. It fetches data from exchanges and analyzes the data for potential bullish indicators.","The results are compared with previously issued alerts to reduce redundancy, and are texted to my number."],
+        "proj-tech":"Python, AWS (Hosting), CCXT Library (Data), Twilio Client (Messages) ",
+        "proj-link":"https://github.com/ShaishavShah04/Crypto-Scanner",
+        "proj-img": "src/media/scanner.png",
+    }, {
+        "proj-name": "Portfolio Website",
+        "proj-description":["A script running on a EC2 instance that scans the cryptocurrency markets hourly. It fetches data from exchanges and analyzes the data for potential bullish indicators.","The results are compared with previously issued alerts to reduce redundancy, and are texted to my number."],
+        "proj-tech":"Python, AWS (Hosting), CCXT Library (Data), Twilio Client (Messages) ",
+        "proj-link":"https://github.com/ShaishavShah04/Crypto-Scanner",
+        "proj-img": "./src/media/scanner.png",
     }
     ]
 
     return (
         <div className="projects-page">
-            
+            <h2>My Projects!</h2>
+            <div className="project-container">
+                {ProjectPageInfo.map( function(projObj){
+                    return (
+                        <div className="project-card">
+                            <div className="proj-text">
+                                <h4>{projObj['proj-name']}</h4>
+                                <br/>
+                                {projObj['proj-description'].map( (sentence) => <p>{sentence}<br/><br/></p>  )}
+                                <br/>
+                                <b>Created Using:</b>
+                                <br/>
+                                <p>{projObj['proj-tech']}</p>
+                                <br/>
+                                <a href={projObj['proj-link']}>{<Link/>}</a>
+                            </div>
+                        </div>
+
+                    )
+                } )}
+            </div>
         </div>
     )
 }
