7c4ac623535284a3f6265d4d8095698bb09fcdb9
Shaishav Shah <shaishav.shah04@gmail.com>
2021-12-30 23:10:26 -0700

    Tech section done!

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { BsPlug } from "react-icons/bs";
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies, from back-end to front-end and everything in between!
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            <ul>
+              <li>React.js</li>
+              <li>HTML/CSS</li>
+              <li>JavaScript</li>
+            </ul>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <BsPlug size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            <ul>
+              <li>Express.js</li>
+              <li>Node.js</li>
+              <li>Python</li>
+              <li>C / C++</li>
+            </ul>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Database</ListTitle>
+          <ListParagraph>
+            <ul>
+              <li>MySQL</li>
+              <li>MongoDB</li>
+            </ul>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
