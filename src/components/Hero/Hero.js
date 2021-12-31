91dc5352d9e6a4dc1c4db9d4fc9bab6d5cdb762e
Shaishav Shah <shaishav.shah04@gmail.com>
2021-12-30 22:41:48 -0700

    Hero Section done.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,21 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey!
+        <br />
+        I'm Shaishav Shah.
+      </SectionTitle>
+      <SectionText>
+       Hi! &#128075; I'm an undergraduate Computing Science / Business student at the University of Alberta. I like to design and build applications that save time in the tasks around us!
+      </SectionText>
+      <Button>
+        Resume
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
