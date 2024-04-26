0482d819192f21b7af26dd99d9739e395167d7fd
Shaishav Shah <shaishav.shah04@gmail.com>
2024-04-25 21:52:24 -0600

    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
         I'm Shaishav Shah.
       </SectionTitle>
       <SectionText>
-       I'm an undergraduate Computing Science / Business student at the University of Alberta. I like to design and build applications that save time in the tasks around us!
+       I like to work on complex problems that save time in the tasks around us!
       </SectionText>
         
       <a href="./ShaishavShahResume.pdf" target="_blank">
